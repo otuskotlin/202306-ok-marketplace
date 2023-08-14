45b47d64299f06678c2a950bcedefc2d5cc638a9
Kirill Krylov <mrm.kikll@gmail.com>
2023-08-14 22:47:10 +0200

    M1l6 models and mappers (#15)
    * M2L6: Internal mappers module
    
    * M2L6: Fixed review comment
    
    * M2L6: add macosArm64 target
    
    ---------
    
    Co-authored-by: Dmitriy Govorukhin <dgovorukhin@samokat.ru>

---
+++ ok-marketplace-mappers-v2/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+group = rootProject.group
+version = rootProject.version
+
+kotlin {
+    jvm {}
+    linuxX64 {}
+    macosX64 {}
+    macosArm64 { }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+
+                implementation(project(":ok-marketplace-api-v2-kmp"))
+                implementation(project(":ok-marketplace-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+
+    }
+}
