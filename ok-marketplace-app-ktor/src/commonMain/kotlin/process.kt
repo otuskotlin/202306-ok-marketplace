3ca9217566464d4f395f5a24de5c123e6c1356de
Sergey Okatov <sokatov@gmail.com>
2023-09-29 18:24:42 +0500

    m4l3 Бизнес-логика приложения: удалены лишние файлы

--- ok-marketplace-app-ktor/src/commonMain/kotlin/process.kt
+++
@@ -1,8 +0,0 @@
-package ru.otus.otuskotlin.marketplace.app
-
-import ru.otus.otuskotlin.marketplace.biz.MkplAdProcessor
-import ru.otus.otuskotlin.marketplace.common.MkplContext
-
-private val processor = MkplAdProcessor()
-suspend fun process(ctx: MkplContext) = processor.exec(ctx)
-
